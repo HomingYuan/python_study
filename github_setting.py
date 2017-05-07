881660399e7005b06542b57eb1abde46131fc7e0
Homing <470034235@qq.com>
2017-05-07 19:44:30 +0800

    second commit

---
+++ github_setting.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Homing
+@software: PyCharm Community Edition
+@file: github_setting.py
+@time: 2017/5/7 19:40
+"""
+# github 远程配置与提交
+
+# 本地建一个仓库
+
+'''
+git init
+
+git add . # 添加所有文件
+
+git commit -m "first commit"
+
+git remote add origin https://github.com/HomingYuan/python_study.git
+
+git push -u origin master
+
+
+'''
+
+
+
+
+
+
+
+
+
